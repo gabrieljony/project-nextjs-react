53b9552a3f26c800d2106cdd375ae45a57568953
Gabriel Jony <gabjony@gmail.com>
2020-05-23 22:12:59 -0300

    Configuração da senha do stripe

---
+++ config/stripe.js
@@ -0,0 +1,5 @@
+export default {
+  // Get your test API keys https://stripe.com/en-br
+  publicKey: 'pk_test_Iv7AKpCLM693OsTL2Ci6wlwH00SqP8omIN',
+  secretKey: 'sk_test_ucHmPCqqDkIWwUst3YFCCAyV00p9EF3hKh',
+};
